1a3ae5a014bb5cac10d7a2e44a0914c73bcce15b
DABAKER165 <39739223+DABAKER165@users.noreply.github.com>
2022-09-23 13:42:55 -0500

    Initial commit

---
+++ matched_pairs_bbduk_wrapper.py
@@ -0,0 +1,249 @@
+from Bio import SeqIO
+from Bio.SeqIO.QualityIO import FastqGeneralIterator
+import os
+from argparse import ArgumentParser, ArgumentTypeError
+from subprocess import Popen, PIPE, run as subprocess_run
+import gzip
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise ArgumentTypeError('Boolean value expected.')
+
+
+def parse_process_arrays_args(parser: ArgumentParser):
+    """Parses the python script arguments from bash and makes sure files/inputs are valid"""
+    parser.add_argument('--primer_fasta',
+                        type=str,
+                        help='where fasta primer, headers must be nomenclature: \n\
+                        <primer_name>_<primer_group>_<primer_direction>_<optional_additional_primer>\n\
+                        or\n\
+                        <primer_name>_<primer_group>_<primer_direction>',
+                        required=True)
+    parser.add_argument('--in_path',
+                        type=str,
+                        help='input path to forward reads.',
+                        required=True)
+    parser.add_argument('--in2_path',
+                        type=str,
+                        help='input path to reverse-complement paired reads.',
+                        required=True)
+
+    parser.add_argument('--outm',
+                        type=str,
+                        help='output path to output_path forwared reads',
+                        required=True)
+    parser.add_argument('--outm2',
+                        type=str,
+                        help='output path to reverse-complement paired reads',
+                        required=True)
+    parser.add_argument('--temp_dir',
+                        type=str,
+                        help='path to intermediate files',
+                        default=None,
+                        required=False)
+    parser.add_argument('--mem',
+                        type=int,
+                        help='ram to allocate in MB',
+                        default=8000,
+                        required=False)
+    parser.add_argument('--bbmap_dir',
+                        type=str,
+                        help='dir where bbmap tools are located',
+                        default='/bin/bbmap',
+                        required=False)
+    parser.add_argument('--ktrim',
+                        type=str,
+                        help='set to f if you want to leave the primers, t to trim the primers',
+                        default='f',
+                        required=False)
+
+
+def get_process_arrays_args():
+    """	Inputs arguments from bash
+    Gets the arguments, checks requirements, returns a dictionary of arguments
+    Return: args - Arguments as a dictionary
+    """
+    parser = ArgumentParser()
+    parse_process_arrays_args(parser)
+    args = parser.parse_args()
+    return args
+
+
+args = get_process_arrays_args()
+# same arguments to a local variable by same name as the argument
+primer_fasta = args.primer_fasta
+in_path = args.in_path
+in2_path = args.in2_path
+outm = args.outm
+outm2 = args.outm2
+temp_dir = args.temp_dir
+mem = args.mem
+bbmap_dir = args.bbmap_dir
+ktrim = args.ktrim
+if temp_dir is None:
+    temp_dir = os.path.join(os.path.dirname(outm), 'TMP')
+
+# way to concat fastq.gz in python, might not be the most efficient
+def concat_fastq_gz(input_fastq_filepath, out_file):
+    fastq_sequences = FastqGeneralIterator(gzip.open(input_fastq_filepath, "rt"))
+    for (f_id, f_seq, f_q) in fastq_sequences:
+        out_file.write('@{0}\n{1}\n+\n{2}\n'.format(f_id, f_seq, f_q))
+
+
+def fasta_groups_to_dict(fasta_path):
+
+    primer_fasta_dict = {}
+    fasta_sequences = SeqIO.parse(open(fasta_path), 'fasta')
+    for fasta in fasta_sequences:
+        header_name, sequence = fasta.id, str(fasta.seq)
+        header_split = header_name.split('_')
+        # print(header_split)
+        # primer_name = header_split[0]
+        primer_group = header_split[1]
+        primer_direction = header_split[2]
+        if primer_group not in primer_fasta_dict.keys():
+            primer_fasta_dict[primer_group] = {}
+        #print(primer_fasta_dict)
+        if primer_direction not in primer_fasta_dict[primer_group]:
+            primer_fasta_dict[primer_group][primer_direction] = []
+        fastq_list = primer_fasta_dict[primer_group][primer_direction]
+        fastq_list.append((header_name, sequence))
+        primer_fasta_dict[primer_group][primer_direction] = fastq_list
+    return primer_fasta_dict
+
+
+def dict_to_grouped_fastas(grouped_fasta_dir,
+                           grouped_fasta_dict):
+    primer_group_path_dict ={}
+    for primer_group, primer_direction_dict in grouped_fasta_dict.items():
+        for primer_direction, fasta_data_list in primer_direction_dict.items():
+            grouped_ref_path = os.path.join(grouped_fasta_dir, '{0}_{1}.fasta'.format(primer_group, primer_direction))
+            if primer_group not in primer_group_path_dict.keys():
+                primer_group_path_dict[primer_group] = {}
+            primer_group_path_dict[primer_group][primer_direction] = grouped_ref_path
+            with open(grouped_ref_path, 'w') as out_file:
+                for fasta_data in fasta_data_list:
+                    out_file.write('>{0}\n'.format(fasta_data[0]))
+                    out_file.write('{0}\n'.format(fasta_data[1]))
+
+    return primer_group_path_dict
+# create the required directories for outputting files
+os.makedirs(temp_dir, exist_ok=True)
+grouped_fasta_dir = os.path.join(temp_dir,'single_fasta')
+os.makedirs(grouped_fasta_dir, exist_ok=True)
+# Create grouped fasta files for the forward and reverse primer pairs.
+# create an iterable dictionary of paths.
+grouped_fasta_dict = fasta_groups_to_dict(primer_fasta)
+primer_group_path_dict = dict_to_grouped_fastas(grouped_fasta_dir, grouped_fasta_dict)
+print('Primer Group Count:', len(primer_group_path_dict.keys()))
+
+left_outpath_list = []
+right_outpath_list = []
+primer_group_count = len(primer_group_path_dict.keys())
+primer_group_count = len(primer_group_path_dict.keys())
+i = 1
+for primer_group, primer_dict in primer_group_path_dict.items():
+    print('Processing Primer Group {0} of {1} -- name:{2}'.format(i, primer_group_count, primer_group))
+    # create intermediate/temporary file names to be used by bbduk and repair sh to conduct the primer matching
+    i += 1
+    in_fasta_left_path = primer_dict['LEFT']
+    base_left_fasta = os.path.basename(in_fasta_left_path)[:-6]
+    in_fasta_right_path = primer_dict['RIGHT']
+    base_right_fasta = os.path.basename(in_fasta_right_path)[:-6]
+    fasta_left_temp_1 = os.path.join(temp_dir, '{}_temp_1.fastq.gz'.format(base_left_fasta))
+    fasta_left_temp_2 = os.path.join(temp_dir, '{}_temp_2.fastq.gz'.format(base_left_fasta))
+    fasta_left_temp_3 = os.path.join(temp_dir, '{}_temp3.fastq.gz'.format(base_left_fasta))
+    left_outpath_list.append(fasta_left_temp_3)
+    fasta_right_temp_1 = os.path.join(temp_dir, '{}_temp_1.fastq.gz'.format(base_right_fasta))
+    fasta_right_temp_2 = os.path.join(temp_dir, '{}_temp_2.fastq.gz'.format(base_right_fasta))
+    fasta_right_temp_3 = os.path.join(temp_dir, '{}_temp3.fastq.gz'.format(base_right_fasta))
+    right_outpath_list.append(fasta_right_temp_3)
+    # run bbduk on the forward primer set of the group
+    print('Running bbduk left: ', primer_group)
+    ssh_cmd_out = subprocess_run(['java', '-ea',
+                                  '-Xmx{0}m'.format(mem),
+                                  '-Xms{0}m'.format(mem),
+                                  '-cp', '{0}/current/'.format(bbmap_dir),
+                                  'jgi.BBDuk',
+                                  'in={}'.format(in_path),
+                                  'outm={}'.format(fasta_left_temp_1),
+                                  'ref={0}'.format(in_fasta_left_path),
+                                  'ktrim={}'.format(ktrim),
+                                  'k=21',
+                                  'qtrim=f',
+                                  'trimq=30',
+                                  'hdist=3',
+                                  'rcomp=f',
+                                  'minlength=75',
+                                  'restrictleft=32',
+                                  'overwrite=t'],
+                                 shell=False,
+                                 stdout=PIPE,
+                                 stderr=PIPE)
+    print(ssh_cmd_out)
+    # run repair again as the threads can put the reads out of order and will be missing in bbduk
+    print('Running repair left: ', primer_group)
+    ssh_cmd_out = subprocess_run(['{0}/repair.sh'.format(bbmap_dir),
+                                  'in={}'.format(fasta_left_temp_1),
+                                  'in2={}'.format(in2_path),
+                                  'out={}'.format(fasta_left_temp_2),
+                                  'out2={}'.format(fasta_right_temp_1)],
+                                 shell=False,
+                                 stdout=PIPE,
+                                 stderr=PIPE)
+    print(ssh_cmd_out)
+    # Run bbduk on the reversecomplement matched pair primer of the group
+    print('Running bbduk right: ', primer_group)
+    ssh_cmd_out = subprocess_run(['java', '-ea',
+                                  '-Xmx{0}m'.format(mem),
+                                  '-Xms{0}m'.format(mem),
+                                  '-cp', '{0}/current/'.format(bbmap_dir),
+                                  'jgi.BBDuk',
+                                  'in={}'.format(fasta_right_temp_1),
+                                  'outm={}'.format(fasta_right_temp_2),
+                                  'ref={0}'.format(in_fasta_right_path),
+                                  'ktrim={}'.format(ktrim),
+                                  'k=21',
+                                  'qtrim=f',
+                                  'trimq=30',
+                                  'hdist=3',
+                                  'rcomp=f',
+                                  'minlength=75',
+                                  'restrictleft=32',
+                                  'overwrite=t'],
+                                 shell=False,
+                                 stdout=PIPE,
+                                 stderr=PIPE)
+    print(ssh_cmd_out)
+    # run repair again as the threads can put the reads out of order and will be missing in bbduk
+    print('Running repair right: ', primer_group)
+    ssh_cmd_out = subprocess_run(['{0}/repair.sh'.format(bbmap_dir),
+                                  'in={}'.format(fasta_left_temp_2),
+                                  'in2={}'.format(fasta_right_temp_2),
+                                  'out={}'.format(fasta_left_temp_3),
+                                  'out2={}'.format(fasta_right_temp_3)],
+                                 shell=False,
+                                 stdout=PIPE,
+                                 stderr=PIPE)
+    print(ssh_cmd_out)
+
+# concatenate the  primer grouped fastq files into a single file for each direction
+print('Concatenating files to create a single file')
+with gzip.open(outm, 'wt') as out_file:
+    for left_outpath in left_outpath_list:
+        concat_fastq_gz(left_outpath, out_file)
+        print(left_outpath)
+
+with gzip.open(outm2, 'wt') as out_file:
+    for right_outpath in right_outpath_list:
+        concat_fastq_gz(right_outpath, out_file)
+        print(right_outpath)
+
+
+
---
+++ readme.md
@@ -0,0 +1,190 @@
+# BBDuk primer matching
+- This repository is a wrapper for the bbmap/bbtools bbduk to improvement of identifying valid reads based on the primers
+  - reference: https://jgi.doe.gov/data-and-tools/software-tools/bbtools/
+- As ran per the documentation, the bbduk will do a good job of identifying (and optionally trimming) primers based on an inputed primer list
+- However, for paired reads the existing workflow does not identify the reads based on: 
+  1. primer being evident in both of the paired reads per the matching criteria
+  2. The paired reads both having the correct matched-paired primer 
+- This workflow loops through the primer pair(s) and runs bbduk/repair.sh in order to meet the above criteria.
+
+## Purpose
+- This mitigates keeping reads that have 1 or 2 matched primers, but primers come from different primer pair/groupss.
+- Most often these reads are a PCR chimera artifact, and should be excluded
+- Since many are PCR chimeras, they are repeated artifacts
+  - These invalid reads can be as high as 20% in some systems.
+  - This means calling artifacts as variants at the >10% allele fraction.
+- Filtering by length does not always work (setting min and max lengths) 
+  - amplicons can be different lengths 
+  - difficult to set a per-amplicon filtering criteria
+  - pcr chimera can have a similar length.
+
+## Use Case
+- Using a matched-pair primer system to conduct genomic sequencing.
+- Using matched-pair reads
+- With the proper settings this workflow can be used for a variety sequencing techniques, and is not bound to a single vendor.
+- 
+# Requirements
+- Linux or MacOsx (Windows OS will need modifications to run)
+- python 3.6-3.8 (later versions are untested)
+- bbtools (bbduk, bbmerge, bbmap, and repair.sh) installed to an excecutable location
+  - https://jgi.doe.gov/data-and-tools/software-tools/bbtools/bb-tools-user-guide/bbduk-guide/
+  - 
+## How to Run:
+- Download the files from this repository
+- bbduk and repair.sh settings are hardcoded in the matched_pairs_bbduk_wrapper.py. Please modify the script as needed.
+- Shown below is example files downloaded to /Volumes/drive1/bbduk_primer_matching/example
+
+```shell
+# bbduk and reformat.sh executables in /bin/bbmap, 8 GB ram allocated
+python3 /Volumes/drive1/bbduk_primer_matching/matched_pairs_bbduk_wrapper.py \
+--primer_fasta /Volumes/drive1/bbduk_primer_matching/example/KX601166.2_primers.fasta \
+--in_path /Volumes/drive1/bbduk_primer_matching/example/zikv_dak_R1.fastq.gz \
+--in2_path /Volumes/drive1/bbduk_primer_matching/example/zikv_dak_R2.fastq.gz \
+--outm /Volumes/drive1/bbduk_primer_matching/example/zikv_dak_R1_matched.fastq.gz \
+--outm2 /Volumes/drive1/bbduk_primer_matching/example/zikv_dak_R2_matched.fastq.gz \
+--ktrim f
+# bbmerge executables in /bin/bbmap, 8 GB ram allocated
+java -ea -Xmx8000m -Xms8000m -Djava.library.path=/bin/bbmap/jni/ \
+-cp /bin/bbmap/current/ jgi.BBMerge \
+in=/Volumes/drive1/bbduk_primer_matching/example/zikv_dak_R1_matched.fastq.gz \
+in2=/Volumes/drive1/bbduk_primer_matching/example/zikv_dak_R2_matched.fastq.gz \
+out=/Volumes/drive1/bbduk_primer_matching/example/zikv_dak_matched_merge.fastq.gz
+# bbmap executables in /bin/bbmap, 8 GB ram allocated
+java -ea -Xmx8000m -Xms8000m -cp /bin/bbmap/current/ align2.BBMap build=1 \
+in=/Volumes/drive1/bbduk_primer_matching/example/zikv_dak_matched_merge.fastq.gz \
+ref=/Volumes/drive1/bbduk_primer_matching/example/KX601166.2.fasta \
+outm=/Volumes/drive1/bbduk_primer_matching/example/zikv_dak_matched_merge.bam \
+
+```
+## required python packages:
+- biopython
+- os
+- argparse
+- subprocess
+- gzip
+## Format of the the input primer fasta
+- The primer pairs must be indicated with the following nomenclature in the headers of each read:
+  - <primer_name>_<primer_group>_<primer_direction>_<optional_additional_primer>
+  - OR
+  - <primer_name>_<primer_group>_<primer_direction>
+  - Underscores are not allowed in the primer_name, primer_group
+  - the primer_direction must be 'LEFT' or 'RIGHT'
+
+### Example of input primer fasta:
+```text
+>ZIKVDAK_40_RIGHT
+GCGTCAATATGCTGTTTTGCGT
+>ZIKVDAK_40_LEFT
+TGGTGCGTAGGATCATAGGTGA
+>ZIKVDAK_39_RIGHT
+CTGACTAGCAGGCCTGACAACA
+>ZIKVDAK_39_LEFT
+GCTGTGCCAATTGACTGGGTAC
+```
+## Example of input primer fasta using an alternate primer:
+  - Some systems have a possible mutation in one or more primer regions
+    - The use of alternative primers is required to properly amplify the amplicon.
+```text
+>ZIKVDAK_40_RIGHT
+GCGTCAATATGCTGTTTTGCGT
+>ZIKVDAK_40_LEFT_1
+TGGTGCGTAGGATCATAGGTGG
+>ZIKVDAK_40_LEFT_2
+TGGTGCGTAGGATCATAGGTGA
+>ZIKVDAK_39_RIGHT
+CTGACTAGCAGGCCTGACAACA
+>ZIKVDAK_39_LEFT
+GCTGTGCCAATTGACTGGGTAC
+```
+
+## Notes on Efficiency
+- This workflow is a looping wrapper around BBduk and repair.sh.
+- It is VERY inefficient, introducing the matched pair criteria directly into bbduk would greatly increase the speed substantially
+  - The speed increase would be about (1.5 * primer_pair_count) x, therefore a 40 primer pair system can expect to in <2% of the processing time
+- maybe a match_primers flag
+# Example of good and bad reads 
+  - using Zikv KX601166.2 strain laboratory sample data
+  - MiSeq istrument
+  - "TruSeq" setting with read length = 251
+  - Input Fastq Files have been randomly downsampled by 95% prior to uploading, due to github file size limits.
+  - Publication: 
+    - Block, L.N., Schmidt, J.K., Keuler, N.S. et al. Zika virus impacts extracellular vesicle composition and cellular gene expression in macaque early gestation trophoblasts. Sci Rep 12, 7348 (2022). https://doi.org/10.1038/s41598-022-11275-9
+## Matched-primers paired-read example
+    
+Primers used:
+```text
+>ZIKVDAK_3_RIGHT
+CCGGGGCAATCAACAATATCATGA
+>ZIKVDAK_3_LEFT
+TACAGATCATGGACCTCGGGCA
+```
+Forward Matched-primers read:
+```text
+@M01472:427:000000000-JWD6B:1:1111:9803:23202 1:N:0:1
+TACAGATCATGGACCTCGGGCACATGTGTGACGCCACCATGAGTTATGAGTGCCCCATGCTAGACGAGGGAGTGGAGCCAGATGACGTCGATTGCTGGTGCAACACGACATCGACTTGGGTTGTGTACGGAACCTGTCATCATAAAAAAGGTGAAGCACGACGATCCAGAAGAGCCGTGACGCTTCCTTCTCACTCCACGAGGAAGCTGCAAACGCGATCGCAGACTTGGCTAGAGTCAAGAGAATACACA
++
+DDDDDFFFFFFFGGGGGGGGGGGHHHFHHHHHGGGGGHHHHHHHHHHHHHHGHHHGGHHHHHHHHGEGGGGGGHGHHHGHHHHFHHGGGGGGHGHHHHHHHHHHHGGGGGGHGHGGGHHHGGGGGHHHHGGGGGHHHHHHHHHHHHHHGGGGHHHHHHHGGGGGGHGHHHHHHHHHGFGGGGGFGGGGGGGGGGGGGGGGGFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+
+```
+Reverse complement Matched-primers read:
+```text
+@M01472:427:000000000-JWD6B:1:1111:9803:23202 2:N:0:1
+CCGGGGCAATCAACAATATCATGACCAAGTATATAACTTTTTGGCTCGTCGAGCTTCCCAGGAGCCAGGCTATAGCTACAGCCACTAGCGCAAACCCGGGGTTCCTGAATATCCAATTTTCAACCTTGATCAGGTGCTTTGTGTATTCTCTTGACTCTAGCCAAGTCTGCGATCGCGTTTGCAGCTTCCTCGTGGAGTGAGAAGGAAGCGTCACGGCTCTTCTGGATCGTCGTGCTTCACCTTTTTTATGA
++
+BCBBBCCCCFFFGGGGGGGGGGHHHHHGGHHHHHGHHHHHHHGGHHHGFGGGGGGHHHHHHGHGHGHHHHHHGHFHHHHHGHFHHHHFHFEGFGHGGGGGGGGHHHHHHHHHHHHHHHHHHHHHHHHGHHHHHHGHHHHHFHHHHHHHHHHHHHHHHHHHHHHHEFHHGHGGHGCFGFGGCHHHHHHGGFGGGGG?FGGBFGGGGGGGGGGDDFFBDDFFFFFFFFEFBFFAFDAEFFFFFFFFFFFEFF0
+
+```
+Merged Matched-primers read (length = 390:
+@M01472:427:000000000-JWD6B:1:1111:9803:23202 1:N:0:1
+TACAGATCATGGACCTCGGGCACATGTGTGACGCCACCATGAGTTATGAGTGCCCCATGCTAGACGAGGGAGTGGAGCCAGATGACGTCGATTGCTGGTGCAACACGACATCGACTTGGGTTGTGTACGGAACCTGTCATCATAAAAAAGGTGAAGCACGACGATCCAGAAGAGCCGTGACGCTTCCTTCTCACTCCACGAGGAAGCTGCAAACGCGATCGCAGACTTGGCTAGAGTCAAGAGAATACACAAAGCACCTGATCAAGGTTGAAAATTGGATATTCAGGAACCCCGGGTTTGCGCTAGTGGCTGTAGCTATAGCCTGGCTCCTGGGAAGCTCGACGAGCCAAAAAGTTATATACTTGGTCATGATATTGTTGATTGCCCCGG
++
+DDDDDFFFFFFFGGGGGGGGGGGHHHFHHHHHGGGGGHHHHHHHHHHHHHHGHHHGGHHHHHHHHGEGGGGGGHGHHHGHHHHFHHGGGGGGHGHHHHHHHHHHHGGGGGGHGHGGGHHHGGGGGHHHHGGGGGHHHHHJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJHHHHGHHHHHHGHHHHHHHHHHHHHHHHHHHHHHHHGGGGGGGGHGFGEFHFHHHHFHGHHHHHFHGHHHHHHGHGHGHHHHHHGGGGGGFGHHHGGHHHHHHHGHHHHHGGHHHHHGGGGGGGGGGFFFCCCCBBBCB
+
+## Mismatched primer paired-read example
+- This has a primer from two different primer groups on the ends
+- It is likely a pcr chimera.
+Primers used:
+```text
+>ZIKVDAK_11_RIGHT
+CCTTTCCCTTGACAGCTGTTCC
+>ZIKVDAK_3_LEFT
+TACAGATCATGGACCTCGGGCA
+```
+Forward Mismatched primer read:
+```
+@M01472:427:000000000-JWD6B:1:1102:12301:25336 1:N:0:1
+TACAGATCATGGACCTCGGGCACATGTGTGACGCCACCATGAGTTATGAGTGCCCCATGCTAGACGAGGGAGTGGAGCCAGATGACGTCGATTGCTGGTGCAACACGACATCGACTTGGGTTGTGTACGGAACCTGTCATCATAAAAAAGGTGAAGCACGACGATCCAGAAGAGCCGTGACGCTTCCTTGTGGAGGATCACGGGTTTGGGATCTTCCACACCAGTGTTTGGCTGAAGGTCAGAGAGGACTA
++
+CCCCBFFFFFFFGGGGGGGGGGGHHHHHHHHHGGGGGHHHHHHHHHHHGHHHHHHGGHHHHHHHHGGGGGEGGHGHHHGHHHHHHHGGGGGGHGHHHGGGHHHHHGGGGGGHGHFGGHHHDFGGGHHHHGGGGGHHHHHHHHHHHHHHCDGFGHHFGHHGGGGGGHFCHHHHHHGHGGAEGGGGGGGGGGGGGGGGGGGGFFFF@AFFEDFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
+```
+Reverse complement Mismatched primer read
+```
+@M01472:427:000000000-JWD6B:1:1102:12301:25336 2:N:0:1
+CCTTTCCCTTGACAGCTGTTCCTATGACGGCTGGGTCACACTCTAGTGAGTAGTCCTCTCTGACCTTCAGCCAAACACTGGTGTGGAAGATCCCAAACCCGTGATCCTCCACAAGGAAGCGTCACGGCTCTTCTGGATCGTCGTGCTTCACCTTTTTTATGATGACAGGTTCCGTACACAACCCAAGTCGATGTCGTGTTGCACCAGCAATCGACGTCATCTGGCTCCACTCCCTCGTCTAGCATGGGGCC
++
+ABBCCFFFFFFFGGFGGGGGGGGHHHGHGGGGGGGHHHHHHHHHGFHAFEGHHHGHHHHHHHEHHHHHHGHGHGHGHGFHHHHHGGHHGHHFHHHHHHGGGGFGHHHHHHHGHEHEHHHHGGGGGGGGGGHHHHHHGHHGHHGGGGGHGHHHHHEHHGHHHHHGHHHHGGHHHGHHGGHFFGGGGHGHHGGGHHGHGCEFEFF0CFGGGGGFEFBCDGGGGGFFFFFFFF/FBE/B>FFFEFFFFFFFFF-
+
+```
+Merged Mismatched primer read=301:
+```text
+@M01472:427:000000000-JWD6B:1:1102:12301:25336 1:N:0:1
+TACAGATCATGGACCTCGGGCACATGTGTGACGCCACCATGAGTTATGAGTGCCCCATGCTAGACGAGGGAGTGGAGCCAGATGACGTCGATTGCTGGTGCAACACGACATCGACTTGGGTTGTGTACGGAACCTGTCATCATAAAAAAGGTGAAGCACGACGATCCAGAAGAGCCGTGACGCTTCCTTGTGGAGGATCACGGGTTTGGGATCTTCCACACCAGTGTTTGGCTGAAGGTCAGAGAGGACTACTCACTAGAGTGTGACCCAGCCGTCATAGGAACAGCTGTCAAGGGAAAGG
++
+CCCCBFFFFFFFGGGGGGGGGGGHHHHHHHHHGGGGGHHHHHHHHHHHGH<JJJJJJJJJJJJJJJJJJJHJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJEFAHFGHHHHHHHHHGGGGGGGHGHHHGGGGGGGGFGGFFFFFFFCCBBA
+```
+### Evidence of chimera in mismatched read:
+```text
+Merged Read
+2915 - 3035
+# Forward
+TACAGATCATGGACCTCGGGCACATGTGTGACGCCACCATGAGTTATGAGTGCCCCATGCTAGACGAGGGAGTGGAGCCAGATGACGTCGATTGCTGGTGCAACACGACATCGACTTGGGTTGTGTACGGAACCTGTCATCATAAAAAAGGTGAAGCACGACGATCCAGAAGAGCCGTGACGCTTCCTTGTGGAGGATCACGGGTTTGGGATCTTCCACACCAGTGTTTGGCTGAAGGTCAGAGAGGACTA
+# Reverse complement
+                                                  GGCCCCATGCTAGACGAGGGAGTGGAGCCAGATGACGTCGATTGCTGGTGCAACACGACATCGACTTGGGTTGTGTACGGAACCTGTCATCATAAAAAAGGTGAAGCACGACGATCCAGAAGAGCCGTGACGCTTCCTTGTGGAGGATCACGGGTTTGGGATCTTCCACACCAGTGTTTGGCTGAAGGTCAGAGAGGACTACTCACTAGAGTGTGACCCAGCCGTCATAGGAACAGCTGTCAAGGGAAAGG
+# Merged read
+TACAGATCATGGACCTCGGGCACATGTGTGACGCCACCATGAGTTATGAGTGCCCCATGCTAGACGAGGGAGTGGAGCCAGATGACGTCGATTGCTGGTGCAACACGACATCGACTTGGGTTGTGTACGGAACCTGTCATCATAAAAAAGGTGAAGCACGACGATCCAGAAGAGCCGTGACGCTTCCTTGTGGAGGATCACGGGTTTGGGATCTTCCACACCAGTGTTTGGCTGAAGGTCAGAGAGGACTACTCACTAGAGTGTGACCCAGCCGTCATAGGAACAGCTGTCAAGGGAAAGG                                                                                          
+# KX601166.2 551-740
+TACAGATCATGGACCTCGGGCACATGTGTGACGCCACCATGAGTTATGAGTGCCCCATGCTAGACGAGGGAGTGGAGCCAGATGACGTCGATTGCTGGTGCAACACGACATCGACTTGGGTTGTGTACGGAACCTGTCATCATAAAAAAGGTGAAGCACGACGATCCAGAAGAGCCGTGACGCTTCCTT
+# KX601166.2 2915-3035
+                                                                                                                                                                                     GCTTCCTTGTGGAGGATCACGGGTTTGGGATCTTCCACACCAGTGTTTGGCTGAAGGTCAGAGAGGACTACTCACTAGAGTGTGACCCAGCCGTCATAGGAACAGCTGTCAAGGGAAAGG
+
+```
